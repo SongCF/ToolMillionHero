daf4b61029b268fcaa384e518c7e0551b37d5603
songchangfu <songchangfu@xiaoduotech.com>
2018-01-11 13:00:48 +0800

    readme

--- baidu/ocr/ocr.go
+++ baidu/ocr/ocr.go
@@ -5,9 +5,15 @@ import (
 	"fmt"
 	"github.com/SongCF/ToolMillionHero/baidu/auth"
 	"github.com/SongCF/ToolMillionHero/utils"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/url"
+	"os"
 )
 
 //网络图片文字识别
@@ -48,15 +54,52 @@ func loadImageBytes(filename string) ([]byte, error) {
 	return ioutil.ReadFile(filename)
 }
 
-//func loadImage(filename string) image.Image {
+//func loadClipImageBytes(filename string) ([]byte, error) {
+//	const w, h, x0, y0 = 1080, 1920, 50, 700
+//	const x1, y1 = w - x0, h - 280
 //	inFile, err := os.Open(filename)
+//	defer inFile.Close()
 //	if err != nil {
 //		panic(err)
 //	}
-//	src, err := png.Decode(inFile)
+//	err = clip(inFile, fOut, x0, y0, x1, y1, 100)
 //	if err != nil {
 //		panic(err)
 //	}
-//	inFile.Close()
-//	return src
+//}
+//
+//func clip(in io.Reader, out io.Writer, x0, y0, x1, y1, quality int) error {
+//	origin, fm, err := image.Decode(in)
+//	if err != nil {
+//		return err
+//	}
+//
+//	switch fm {
+//	case "jpeg":
+//		img := origin.(*image.YCbCr)
+//		subImg := img.SubImage(image.Rect(x0, y0, x1, y1)).(*image.YCbCr)
+//		return jpeg.Encode(out, subImg, &jpeg.Options{quality})
+//	case "png":
+//		switch canvas.(type) {
+//		case *image.NRGBA:
+//			img := canvas.(*image.NRGBA)
+//			subImg := img.SubImage(image.Rect(x0, y0, x1, y1)).(*image.NRGBA)
+//			return png.Encode(out, subImg)
+//		case *image.RGBA:
+//			img := canvas.(*image.RGBA)
+//			subImg := img.SubImage(image.Rect(x0, y0, x1, y1)).(*image.RGBA)
+//			return png.Encode(out, subImg)
+//		}
+//	case "gif":
+//		img := origin.(*image.Paletted)
+//		subImg := img.SubImage(image.Rect(x0, y0, x1, y1)).(*image.Paletted)
+//		return gif.Encode(out, subImg, &gif.Options{})
+//	case "bmp":
+//		img := origin.(*image.RGBA)
+//		subImg := img.SubImage(image.Rect(x0, y0, x1, y1)).(*image.RGBA)
+//		return bmp.Encode(out, subImg)
+//	default:
+//		return errors.New("ERROR FORMAT")
+//	}
+//	return nil
 //}
